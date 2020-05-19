02bb481add53663cc4010a0b862cd8f021988f47
znzmektm2 <tingcber2@naver.com>
2020-05-20 01:22:24 +0900

    자바스크립트 함수 비동기 로딩, React.lazy와 Suspense를 통한 컴포넌트 코드 스플리팅, loadable Component를 통한 코드 스플리팅

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import loadable from '@loadable/component';
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>loading...</div>
+});
+const onMouseOver = () => {
+  SplitMe.preload();
+}
+
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={onClick} onMouseOver={onMouseOver}>Hello React!</p>
+        {visible && <SplitMe/>}
+      </header>
+    </div>
+  );
+}
+
+export default App;
